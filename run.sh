c13953912de88f8a96b1f437814e4dd4a30cd3d0
Joan Fabrégat <joan@codeinc.co>
2024-07-18 00:05:49 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
 timestamp=$(date -u +"%Y-%m-%dT%H%M%SZ")
+destination="/tmp/$timestamp.sql.gz"
+s3_destination="s3://$S3_BUCKET$S3_PREFIX/$timestamp.sql.gz"
 
 # Dump MySQL database
-mysqldump -h $MYSQL_HOST -P $MYSQL_PORT -u $MYSQL_USER -p$MYSQL_PASSWORD --no-tablespaces $MYSQL_DATABASE > /tmp/$MYSQL_DATABASE.sql | gzip -c > /tmp/$MYSQL_DATABASE.sql.gz
+echo "Dumping MySQL database $MYSQL_DATABASE"
+mysqldump -h "$MYSQL_HOST" -P "$MYSQL_PORT" -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" --no-tablespaces "$MYSQL_DATABASE" | gzip -c > "$destination"
+echo "Dumped MySQL database $MYSQL_DATABASE (file size: $(du -sh "$destination" | cut -f1))"
 
 # Upload to S3
-aws s3 cp /tmp/$MYSQL_DATABASE.sql.gz s3://$S3_BUCKET$S3_PREFIX${timestamp}.sql.gz
+echo "Uploading dump to S3"
+aws s3 cp "$destination" "$s3_destination"
+echo "Uploaded dump to S3 ($s3_destination)"
 
 # Remove dump
-rm /tmp/$MYSQL_DATABASE.sql.gz
+echo "Removing dump"
+rm "$destination"
