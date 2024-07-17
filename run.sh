781f3d41959be9cd2762e76b5703c23d3bfaf626
Joan Fabrégat <joan@codeinc.co>
2024-07-17 23:30:43 +0200

    bug fix

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@
 mysqldump -h $MYSQL_HOST -P $MYSQL_PORT -u $MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DATABASE > /tmp/$MYSQL_DATABASE.sql | gzip -c > /tmp/$MYSQL_DATABASE.sql.gz
 
 # Upload to S3
-aws s3 cp /tmp/$MYSQL_DATABASE.sql.gz s3://mybucket/$MYSQL_DATABASE.sql.gz
+aws s3 cp /tmp/$MYSQL_DATABASE.sql.gz s3://$S3_BUCKET$S3_PREFIX$MYSQL_DATABASE.sql.gz
 
 # Remove dump
 rm /tmp/$MYSQL_DATABASE.sql.gz
